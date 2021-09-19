15ee4b4d333b309053d408dc52ceef90029b0ddc
Yongjoo Lee <oyongjoo@gmail.com>
2021-09-19 21:57:07 +0900

    integer_literal_oct_hex_suffix

--- week_2/literal/main.c
+++ week_2/literal/main.c
@@ -5,14 +5,10 @@
 
 int main()
 {
-    printf("%ld\n", -10L);                       // long 크기의 정수 리터럴
-    printf("%lld\n", -1234567890123456789LL);    // long long 크기의 정수 리터럴
-
-    printf("%u\n", 10U);                         // unsigned int 크기의 정수 리터럴
-    printf("%lu\n", 1234567890UL);               // unsigned long 크기의 정수 리터럴
-
-    printf("%lu\n", 10UL);                       // unsigned long 크기의 정수 리터럴
-    printf("%llu\n", 1234567890123456789ULL);    // unsigned long long 크기의 정수 리터럴
+    printf("0%lo\n", 017L);             // long 크기의 8진 정수 리터럴
+    printf("0%lo\n", 017UL);            // unsigned long 크기의 8진 정수 리터럴
+    printf("0x%lX\n", 0x7FFFFFL);       // long 크기의 16진 정수 리터럴
+    printf("0x%lX\n", 0xFFFFFFFFUL);    // unsigned long 크기의 16진 정수 리터럴
 
     return 0;
 }
