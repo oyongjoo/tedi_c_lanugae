1d9c0bdae239eac96dd7ce57e7233f18688beb98
Yongjoo Lee <oyongjoo@gmail.com>
2021-09-19 21:37:33 +0900

    add week_2 literal

---
+++ week_2/literal/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	printf("%d\n", 10);
+	printf("%f\n", 0.1f);
+	printf("%c\n", 'a');
+	printf("%s\n", "Hello, world!");
+	
+	return 0;
+}
