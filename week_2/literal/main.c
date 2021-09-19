8a33ad26e843d2c0fae89bb1765a7ce817ed2d8a
Yongjoo Lee <oyongjoo@gmail.com>
2021-09-19 21:49:51 +0900

    const_assign_error

--- week_2/literal/main.c
+++ week_2/literal/main.c
@@ -5,11 +5,11 @@
 
 int main()
 {
-    const int con1 = 1;         // 상수. 선언과 동시에 초기화
-    const float con2 = .2f;    // 상수. 선언과 동시에 초기화
-    const char con3 = 'a';      // 상수. 선언과 동시에 초기화
-
-    printf("%d %f %c\n", con1, con2, con3);    // 1 0.100000 a
+    const int con1 = 1;         
+    
+    con1 = 2;
+    
+    printf("%d\n", con1);
 
     return 0;
 }
