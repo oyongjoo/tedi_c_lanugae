1c6ef9de83dc4e1b93ffcdbd5eb8729528d927be
Yongjoo Lee <oyongjoo@gmail.com>
2021-09-19 20:57:52 +0900

    char linefeed

--- week_1/char_zero/main.c
+++ week_1/char_zero/main.c
@@ -2,8 +2,10 @@
 
 int main()
 {
-    char c1 = 'a';    // 문자 a 저장
+    char c1 = 'a';			// 문자 a 할당
+    char c2 = 'b';			// 문자 b 할당
+    char linefeed = '\n'; 	// 제어 문자 \n 할당
 
-    printf("%c %d\n", c1+1, c1+1);    // 0 48: 문자 상태로 0이 출력됨
+    printf("%c%c%c%c", c1, linefeed, c2, linefeed);    // 0 48: 문자 상태로 0이 출력됨
     return 0;
 }
