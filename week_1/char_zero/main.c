77569df7526e1c9aacb8b64104550632fe96be17
Yongjoo Lee <oyongjoo@gmail.com>
2021-09-19 21:20:08 +0900

    practice_integer_to_char

--- week_1/char_zero/main.c
+++ week_1/char_zero/main.c
@@ -2,11 +2,10 @@
 
 int main()
 {
-    char c1 = '\t';
-
-    printf("Hello");
-    printf("%c", c1);
-    printf("world\n");
-
+    char c1 = 2;
+    char c2 = 5;
+    
+    printf("%c\n", c1+48);
+    printf("%c\n", c2+48);
     return 0;
 }
