53e3bafda777a675a3262c742a3c44e397aa1e3e
Yongjoo Lee <oyongjoo@gmail.com>
2021-09-19 21:52:08 +0900

    integer literal suffix

--- week_2/literal/main.c
+++ week_2/literal/main.c
@@ -5,11 +5,14 @@
 
 int main()
 {
-    const int con1 = 1;         
-    
-    con1 = 2;
-    
-    printf("%d\n", con1);
+    printf("%ld\n", -10L);                       // long 크기의 정수 리터럴
+    printf("%lld\n", -1234567890123456789LL);    // long long 크기의 정수 리터럴
+
+    printf("%u\n", 10U);                         // unsigned int 크기의 정수 리터럴
+    printf("%lu\n", 1234567890UL);               // unsigned long 크기의 정수 리터럴
+
+    printf("%lu\n", 10UL);                       // unsigned long 크기의 정수 리터럴
+    printf("%llu\n", 1234567890123456789ULL);    // unsigned long long 크기의 정수 리터럴
 
     return 0;
 }
