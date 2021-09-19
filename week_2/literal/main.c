9267480b41d202b542202f3afd1334c0b62a3ab2
Yongjoo Lee <oyongjoo@gmail.com>
2021-09-19 21:59:36 +0900

    real_number_literal_suffix

--- week_2/literal/main.c
+++ week_2/literal/main.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
 int main()
 {
-    printf("0%lo\n", 017L);             // long 크기의 8진 정수 리터럴
-    printf("0%lo\n", 017UL);            // unsigned long 크기의 8진 정수 리터럴
-    printf("0x%lX\n", 0x7FFFFFL);       // long 크기의 16진 정수 리터럴
-    printf("0x%lX\n", 0xFFFFFFFFUL);    // unsigned long 크기의 16진 정수 리터럴
+    printf("%f\n", 0.1f);     // 0.100000: float 크기의 실수 리터럴
+    printf("%f\n", 0.1F);     // 0.100000: float 크기의 실수 리터럴
+    printf("%f\n", 0.1);      // 0.100000: double 크기의 실수 리터럴
+    printf("%Lf\n", 0.1l);    // 0.100000: long double 크기의 실수 리터럴
+    printf("%Lf\n", 0.1L);    // 0.100000: long double 크기의 실수 리터럴
+
+    printf("%f\n", 1.0e-5f);     // 0.000010: float 크기의 실수 리터럴
+    printf("%f\n", 1.0e-5F);     // 0.000010: float 크기의 실수 리터럴
+    printf("%f\n", 1.0e-5);      // 0.000010: double 크기의 실수 리터럴
+    printf("%Lf\n", 1.0e-5l);    // 0.000010: long double 크기의 실수 리터럴
+    printf("%Lf\n", 1.0e-5L);    // 0.000010: long double 크기의 실수 리터럴
 
     return 0;
 }
