9d06b93f1ed1a8e37ba49cb9eccd7a408dd986dd
Yongjoo Lee <oyongjoo@gmail.com>
2021-09-19 21:43:10 +0900

    literal decimal, octal, hexadecimal

--- week_2/literal/main.c
+++ week_2/literal/main.c
@@ -3,11 +3,12 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
-	printf("%d\n", 10);
-	printf("%f\n", 0.1f);
-	printf("%c\n", 'a');
-	printf("%s\n", "Hello, world!");
-	
-	return 0;
-}
+int main()
+{
+	// 10진수는 숫자 그대로 표기하면 되고, 8진수는 숫자 앞에 0을 붙이고, 16진수는 0x를 븥
+    printf("%d\n", 19);        // 19: 10진 정수 리터럴
+    printf("0%o\n", 017);      // 017: 8진 정수 리터럴
+    printf("0x%X\n", 0x1F);    // 0x1F: 16진 정수 리터럴
+
+    return 0;
+}임
