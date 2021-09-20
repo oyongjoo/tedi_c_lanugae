f9ca232663fe18ebbf31aa69398e6347f13c6d3b
Yongjoo Lee <oyongjoo@gmail.com>
2021-09-20 17:38:30 +0900

    scan char

--- week_3/scanf/main.c
+++ week_3/scanf/main.c
@@ -1,27 +1,13 @@
-#define _CRT_SECURE_NO_WARNINGS    // scanf 보안 경고로 인한 컴파일 에러 방지
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <stdlib.h>
-#include <float.h>
 
-int main(int argc, char *argv[]) {
-	float num1;
-	double num2;
-	long double num3;
-		
-	printf("실수를 입력하세요: ");
-	scanf("%f", &num1);		// 실수를 입력받아서 변수에 저장
+int main() {
+	char c1;
 	
-	printf("double 실수를 입력하세요: ");
-	scanf("%lf", &num2);    // 자료형이 double일 때는 %lf
+	printf("문자를 입력하세요: ");
+	scanf("%c", &c1);
 	
-	printf("long double 실수를 입력하세요: ");
-	scanf("%Lf", &num3);    // 자료형이 long double일 때는 %Lf
-	
-	// float와 double은 %f로 출력, long double은 %Lf로 출력
-	printf("%f %f %Lf\n", num1, num2, num3);	// 변수의 내용을 출력
-	
-	// 지수 표기법으로 출력할 때는 float와 double은 %e로 출력, long double은 %Le로 출력
-	printf("%e %e %Le\n", num1, num2, num3); // 3.000000e+05 -1.382700e-02 5.210000e+09
+	printf("%c\n", c1);
 	
 	return 0;
 }
