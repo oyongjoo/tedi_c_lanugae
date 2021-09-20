05e2faf025cd261d1f97cc018a371a9a42e49f96
Yongjoo Lee <oyongjoo@gmail.com>
2021-09-20 22:25:43 +0900

    add sub real number

--- week_3/scanf/main.c
+++ week_3/scanf/main.c
@@ -1,17 +1,11 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main() {
-	int num1, num2, num3;
-	float num4 = 1.7f;
-	double num5 = 232.34234;
-	long double num6 = 12.3234e+102;
+	float num1, num2;
 	
-	printf("정수 세 개를 입력하세요: ");
-	scanf("%d %d %d", &num1, &num2, &num3);
+	num1 = 1.0f + 0.456789f;
+	num2 = 1.0f - 0.456789f;
 	
-	printf("%d\n%d\n%d\n", num1, num2, num3);
-	
-	printf("%f %f %Lf", num4, num5, num6);
-	return 0;
+	printf("%f\n", num1);
+	printf("%f\n", num2);
 }
