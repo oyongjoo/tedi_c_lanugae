d6621cbc64df92d7f4f7c6a01f6f7f829b3095be
Yongjoo Lee <oyongjoo@gmail.com>
2021-09-20 17:30:36 +0900

    real number scanf float, double, long double add expotential

--- week_3/scanf/main.c
+++ week_3/scanf/main.c
@@ -17,7 +17,11 @@ int main(int argc, char *argv[]) {
 	printf("long double 실수를 입력하세요: ");
 	scanf("%Lf", &num3);    // 자료형이 long double일 때는 %Lf
 	
+	// float와 double은 %f로 출력, long double은 %Lf로 출력
 	printf("%f %f %Lf\n", num1, num2, num3);	// 변수의 내용을 출력
+	
+	// 지수 표기법으로 출력할 때는 float와 double은 %e로 출력, long double은 %Le로 출력
+	printf("%e %e %Le\n", num1, num2, num3); // 3.000000e+05 -1.382700e-02 5.210000e+09
 	
 	return 0;
 }
