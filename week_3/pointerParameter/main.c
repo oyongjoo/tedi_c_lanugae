a8e1aa983fd76045c2428b9ddaffb2ae95935873
Yongjoo Lee <oyongjoo@gmail.com>
2021-09-20 13:28:46 +0900

    Parameter is not pointer
    swapNumber를 호출할 때 10이 들어있는 num1과 20이 들어있는 num2를
    넣었습니다. 그 뒤 printf로 값을 출력해보면 num1과 num2의 값이 서로
    바뀌지 않고 10과 20이 그대로 출력되는 것을 볼 수 있습니다. 즉,
    매개변수는 값을 전달하는 역할만 할 뿐 함수 바깥의 변수와는 상관이
    없습니다.

---
+++ week_3/pointerParameter/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swapNumber(int first, int second) {
+	int temp;
+	
+	temp = first;
+	first = second;
+	second = temp;
+}
+
+int main(int argc, char *argv[]) {
+	int num1 = 10;
+	int num2 = 20;
+	
+	swapNumber(num1, num2); // 변수 num1과 num2를 넣어줌
+	
+	printf("%d, %d\n", num1, num2); // 10 20: swapNumber 함수와는 상관없이 
+									// 처음 저장한 10과 20이 출력됨
+	
+	return 0;
+}
---
+++ week_3/scanf/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int num1;
+	
+	printf("정수를 입력하세요: ");
+	scanf("%d", &num1);
+	
+	printf("%d\n", num1);
+	
+	return 0;
+}
