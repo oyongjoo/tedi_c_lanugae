2bdd894a166559e275016730e7720004893ba423
Yongjoo Lee <oyongjoo@gmail.com>
2021-09-20 16:54:57 +0900

    scanf two integer

--- week_3/scanf/main.c
+++ week_3/scanf/main.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
 int main(int argc, char *argv[]) {
-	int num1;
+	int num1, num2;
 	
-	printf("정수를 입력하세요: ");
-	scanf("%d", &num1);
+	printf("정수를 두 개 입력하세요: ");
+	scanf("%d %d", &num1, &num2);	// 값을 두 개 입력받아서 변수 두 개에 저장
 	
-	printf("%d\n", num1);
+	printf("%d %d\n", num1, num2);	// 변수의 내용을 출
 	
 	return 0;
 }
