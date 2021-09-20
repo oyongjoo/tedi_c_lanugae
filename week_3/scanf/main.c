5df7bff9384eaa4f110e8ce75f040577f100bae9
Yongjoo Lee <oyongjoo@gmail.com>
2021-09-20 22:39:07 +0900

    increment decrement char

--- week_3/scanf/main.c
+++ week_3/scanf/main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main() {
-	float num1 = 2.1f;
-	float num2 = 2.1f;
+	char c1 = 'b';
+	char c2 = 'b';
 	
-	num1++;
-	num2--;
+	c1++;	// 문자 자료형 변수의 값을 1 증가시킴, 'c'로 바뀜
+	c2--;	// 문자 자료형 변수의 값을 1 감소시킴, 'a'로 바뀜
 	
-	printf("%f %f\n", num1, num2);
+	printf("%c %c\n", c1, c2);		// c a: b에서 1 증가시켰으므로 c, b에서 1 감소시켰으므로 a
 	
 	return 0;
 }
