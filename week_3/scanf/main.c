2824ee5912ed039b167606822f0f7c62502e24b6
Yongjoo Lee <oyongjoo@gmail.com>
2021-09-20 22:55:15 +0900

    practice increment decrement

--- week_3/scanf/main.c
+++ week_3/scanf/main.c
@@ -3,14 +3,17 @@
 int main() 
 {
     int num1 = 2;
-    int num2 = 2;
+    int num2 = 8;
     int num3;
     int num4;
 
-    num3 = ++num1;    // num1의 값을 num3에 할당한 뒤 num1의 값을 1 증가시킴
-    num4 = --num2;    // num2의 값을 num4에 할당한 뒤 num2의 값을 1 감소시킴
+    num1++;
+    num3 = --num1;
+    
+    --num2;
+    num4 = num2++;
 
-    printf("%d %d\n", num3, num4);    // 2 2
+    printf("%d %d\n", num3, num4);    // 2 7
     printf("%d %d\n", num1, num2);
 
     return 0;
