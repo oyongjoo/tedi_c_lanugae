d4d7b7729d7c2096f4091f9ea84665b92d0dbe26
Yongjoo Lee <oyongjoo@gmail.com>
2021-09-21 13:40:36 +0900

    fmod fmodf fmodl

--- week_3/scanf/main.c
+++ week_3/scanf/main.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() 
 {
-	printf("%d\n", 1 % 3);
-	printf("%d\n", 2 % 3);
-	printf("%d\n", 3 % 3);
-	printf("%d\n", 4 % 3);
-	printf("%d\n", 5 % 3);
-	printf("%d\n", 6 % 3);
+	// 실수의 나머지 연산은 fmod, fmodf, fmodl 함수를 사용
+	
+	double num1 = 10.843;
+	double num2 = 3.79;
+	printf("%f\n", fmod(num1, num2));	// 3.263000
+	
+	float num3 = 10.843f;
+	float num4 = 3.79;
+	printf("%f\n", fmodf(num3, num4));
+	
+	long double num5 = 10.843l;
+	long double num6 = 3.79l;
+	printf("%Lf\n", fmodl(num5, num6));
 	
 	return 0;
 }
