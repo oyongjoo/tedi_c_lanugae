2d2155e0201d54b250447a74b80fd7c1185ab609
Yongjoo Lee <oyongjoo@gmail.com>
2021-09-22 10:46:54 +0900

    ternary conditional operator with conditional expression

--- week_3/if_st/main.c
+++ week_3/if_st/main.c
@@ -6,7 +6,7 @@ int main()
     int num1 = 5;
     int num2;
     
-    num2 = num1 ? 100:200;
+    num2 = (num1 == 10) ? 100:200; // 삼항연산자: num1이 참이면 num2에 100을 할당, 거짓이면 num2에 200을 할당
     
     printf("%d\n", num2);
     
