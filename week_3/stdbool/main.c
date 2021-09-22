c8aca129c7983367d22a618734974e5a65ee8dd5
Yongjoo Lee <oyongjoo@gmail.com>
2021-09-22 13:10:21 +0900

    include stdbool for bool

---
+++ week_3/stdbool/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	bool b1 = true;
+	
+	if (b1 == true)
+		printf("Âü\n", b1);
+	else
+		printf("°ÅÁş\n", b1);
+		
+	return 0;
+}
