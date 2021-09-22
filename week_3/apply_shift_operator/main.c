f241e8d53ee3ea324ac5a7ecca36171bd5777b9e
Yongjoo Lee <oyongjoo@gmail.com>
2021-09-23 00:22:36 +0900

    bitwise_shift_arithmetic_operator_precedence

--- week_3/apply_shift_operator/main.c
+++ week_3/apply_shift_operator/main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-#include <stdbool.h>
- 
+
 int main()
 {
-    int num1;
+    int num1 = 1;
+    int num2 = 2;
+    int num3;
 
-    num1 = 5 == 5 < 10;    // ==보다 <의 우선순위가 높음
+    num3 = num1 << 2 + num2 << 1;    // <<보다 +의 우선순위가 높음 -> num3 = num1 << 4 << 1
 
-    printf("%d\n", num1);    // 0
-
+    printf("%d\n", num3);    // 32
+    
     return 0;
 }
