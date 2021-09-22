2e28162a6561d1f8f7790e809079994ff32205af
Yongjoo Lee <oyongjoo@gmail.com>
2021-09-22 16:01:10 +0900

    bitwise not operator

--- week_3/bitwise/main.c
+++ week_3/bitwise/main.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
 int main() {
-	unsigned char num1 = 1;	// 0000 0001
-	unsigned char num2 = 3;	// 0000 0011
+	unsigned char num1 = 162;	// 1010 0010
+	unsigned char num2;
 	
-	printf("%d\n", num1 & num2);	// 0000 0001: 01과 11을 비트 AND하면 01이 됨
-	printf("%d\n", num1 | num2);	// 0000 0011: 01과 11을 비트 OR하면 11이 됨
-	printf("%d\n", num1 ^ num2);	// 0000 0010: 01과 11을 비트 XOR하면 10이 됨
+	num2 = ~num1;
+	
+	printf("%u\n", num2);	// 93: 0101 1101: num1의 비트 값을 뒤집
 	
 	return 0;
 }
