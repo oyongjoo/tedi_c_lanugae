ccb6bd670daf793b8724b2a9aa394849286d9bfe
Yongjoo Lee <oyongjoo@gmail.com>
2021-09-23 17:41:03 +0900

    while random

--- week_4/while_roof/main.c
+++ week_4/while_roof/main.c
@@ -1,15 +1,14 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>    // srand, rand 함수가 선언된 헤더 파일
+#include <time.h>      // time 함수가 선언된 헤더 파일
 
 int main() {
-	int count;
-	
-	scanf("%d", &count);
+	srand(time(NULL));	//현재 시간값으로 시드 설정
 	
 	int i = 0;
-	while (i < count) {
-		printf("Hello, world! %d\n", i);
-		i++;
+	while(i != 3) {			// 3이 아닐 때 계속 반복
+		i = rand() % 10;
+		printf("%d\n", i);	// rand 함수를 사용하여 무작위로 정수를 생성한 뒤 10 미만의 숫자로 만듦
 	}
 	
 	return 0;
