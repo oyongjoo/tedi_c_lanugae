e894776f93abdc1c1d016607c2e4890982d66e24
Yongjoo Lee <oyongjoo@gmail.com>
2021-09-23 17:04:31 +0900

    while 1 to 100

---
+++ week_4/while_roof/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	int i = 1;
+	
+	while (i <= 100) {
+		printf("Hello, world!\n");
+		i++;
+	}
+	
+	return 0;
+}
