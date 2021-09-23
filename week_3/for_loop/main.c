0e9302b95cabc7f95780333d0ad8692420017d0d
Yongjoo Lee <oyongjoo@gmail.com>
2021-09-23 13:22:19 +0900

    for scan count with new method

--- week_3/for_loop/main.c
+++ week_3/for_loop/main.c
@@ -4,9 +4,9 @@
 int main() {
 	int count;
 	
-	scanf("%d", &count);		//값을 입력받음
+	//scanf("%d", &count);		//값을 입력받음
 	
-	for(; count > 0; count--){
+	for(scanf("%d", &count); count > 0; count--){
 		printf("Hello, world! %d\n", count);
 	}
 	
