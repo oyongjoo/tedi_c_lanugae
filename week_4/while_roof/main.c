2ee55493e045dec4e379b7c3a0fec0487d878996
Yongjoo Lee <oyongjoo@gmail.com>
2021-09-23 21:25:37 +0900

    while infinite loop

--- week_4/while_roof/main.c
+++ week_4/while_roof/main.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>    // srand, rand 함수가 선언된 헤더 파일
-#include <time.h>      // time 함수가 선언된 헤더 파일
 
 int main() {
-	srand(time(NULL));	//현재 시간값으로 시드 설정
-	
-	int i = 0;
-	while(i != 3) {			// 3이 아닐 때 계속 반복
-		i = rand() % 10;
-		printf("%d\n", i);	// rand 함수를 사용하여 무작위로 정수를 생성한 뒤 10 미만의 숫자로 만듦
+	while (1) {		// while에 1을 지정하면 무한루프
+		printf("Hello, world!\n");
 	}
 	
 	return 0;
