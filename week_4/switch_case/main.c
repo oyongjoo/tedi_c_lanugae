3291e69f0372065853bda2a48b20c3b0d14c18bb
Yongjoo Lee <oyongjoo@gmail.com>
2021-09-24 14:13:15 +0900

    TEdi: exercise_02 ternary conditional operator

--- week_4/switch_case/main.c
+++ week_4/switch_case/main.c
@@ -1,11 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main() {
-	int num1;
+	int n, m;
 	
-	scanf("%d", &num1);
-	printf("%s\n", (num1 >= 1 && num1 <= 100) ? "정상" : (num1 > 100) ? "매우 큼":"매우 적음");
-//	printf("%d일\n", date_cnt);
+	scanf("%d %d", &n, &m);
+	
+	printf("%d\n", (n-m) >= 0 ? n-m : -(n-m) );
+	
 	
 	return 0;
 }
