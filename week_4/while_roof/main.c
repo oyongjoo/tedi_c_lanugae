243868be9c9b154fa06411eaf291d788a03bb47e
Yongjoo Lee <oyongjoo@gmail.com>
2021-09-24 11:55:28 +0900

    TedI: to find middle number between 3 numbers

--- week_4/while_roof/main.c
+++ week_4/while_roof/main.c
@@ -1,12 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main() {
-	int i = 0;
+	int a, b, c;
 	
-	do {	// 처음 한 번은 아래 코드가 실행됨
-		printf("Hello, world! %d\n", i);	// Hello, world!와 i의 값을 함께 출력
-		i++;
-	} while (i < 100);		// i가 100보다 작을 때 반복. 0부터 99까지 증가하면서 100번 반복
+	scanf("%d %d %d", &a, &b, &c);
+	
+	int middle;
+	
+	if (a > b) {
+		if (b > c) 
+			middle = b;
+		else {
+			if (c > a)
+				middle = a;
+			else
+				middle = c;
+		}
+	} else {
+		if (a > c) 
+			middle = a;
+		else
+			if (b>c) {
+				middle = c;
+			} else 
+				middle = b;
+	}
+	printf("%d\n", middle);
+	
+	//if( a > b ) {
+//		if (a > b && b > c) {
+//			printf("%d\n", b);
+//		} else if (a > c && c > b) {
+//			printf("%d\n", c);			
+//		} else if (b > a && a > c) {
+//			printf("%d\n", a);
+//		} else if (b > c && c > a) {
+//			printf("%d\n", c);
+//		} else if (c > a && a > b) {
+//			printf("%d\n", a);
+//		} else if (c > b && b > a) {
+//			printf("%d\n", b);
+//		}
+	//}
 	
 	return 0;
 }
