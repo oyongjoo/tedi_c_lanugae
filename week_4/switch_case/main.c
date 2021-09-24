f4e263a2f7b70883985e98c26dc004f930c1786c
Yongjoo Lee <oyongjoo@gmail.com>
2021-09-24 13:44:52 +0900

    TEdI: excercise ternary conditional operator

--- week_4/switch_case/main.c
+++ week_4/switch_case/main.c
@@ -1,39 +1,11 @@
 #include <stdio.h>
 
 int main() {
-	int month;
-	int date_cnt = 0;
+	int num1;
 	
-	scanf("%d", &month);
-	
-	if (month < 1 || month > 12) {
-		printf("오류: 1 ~ 12 사이의 숫자만 입력해 주세요. ");
-		return 1;
-	}
-	
-	switch (month) {
-		case 1:
-		case 3:
-		case 5:
-		case 7:
-		case 8:
-		case 10:
-		case 12:
-			date_cnt = 31;
-			break;
-			
-		case 2:
-			date_cnt = 28;
-			break;
-			
-		case 4:
-		case 6:
-		case 9:
-		case 11:
-			date_cnt = 30;
-			break;
-	}
-	printf("%d일\n", date_cnt);
+	scanf("%d", &num1);
+	printf("%s\n", (num1 >= 1 && num1 <= 100) ? "정상" : (num1 > 100) ? "매우 큼":"매우 적음");
+//	printf("%d일\n", date_cnt);
 	
 	return 0;
 }
