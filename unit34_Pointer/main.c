70d0904add1ca4e879b84a4692d03e019b177a9f
Yongjoo Lee <oyongjoo@gmail.com>
2021-09-26 22:42:06 +0900

    unit34_pointer / EXERCISE: double pointer

--- unit34_Pointer/main.c
+++ unit34_Pointer/main.c
@@ -1,17 +1,17 @@
-#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdint.h>
 
-int main()
-{
+int main() {
 	int *numPtr1;
 	int **numPtr2;
-	int num1 = 10;
+	int num1;
+	
+	scanf("%d", &num1);
 	
 	numPtr1 = &num1;
 	numPtr2 = &numPtr1;
 	
-	**numPtr2 = 20;
-	
-	printf("%d, %d\n", **numPtr2, num1);
+	printf("%d\n", **numPtr2);
 	
 	return 0;
 }
