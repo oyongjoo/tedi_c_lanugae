43a18d3a9c6f2d957fe6c47811ed76e9fc1672aa
Yongjoo Lee <oyongjoo@gmail.com>
2021-09-26 10:38:51 +0900

    if error pattern

--- goto/main.c
+++ goto/main.c
@@ -1,30 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <stdbool.h>
 
-/*
-	변수 num1이 20이 되면 goto를 사용하여 
-	레이블 EXIT로 즉시 이동합니다. 
-	따라서 안쪽과 바깥쪽 루프를 break로 끝낼 필요가 없습니다.	
-	이처럼 goto는 다중 루프를 빠져나올 때 유용합니다. 
-	여기서는 for 반복문을 예로 들었지만 
-	while, do while로 된 중첩 루프도 빠져 나올 수 있습니다.
-*/
 int main() {
-	int num1 = 0;
+	/*
+	** 성별: 남자 1, 여자 2
+	** 나이
+	** 주택 소유 여부: 자가 1, 전월세 0
+	*/
+	int gender, age, isOwner;
 	
-	bool exitOuterLoop = false;
-	for (int i = 0; i < 10; i++) {
-		for (int j = 0; j < 10; j++) {
-			if (num1 == 20) {	// num1이 20이라면
-				goto EXIT;		// 레이블 EXIT로 즉시 이동
-			}
-			
-			num1++;
-		}
+	scanf("%d %d %d", &gender, &age, &isOwner);
+	
+	printf("안녕하세요.\n");
+	printf("문을 연다\n");
+	
+	if (gender == 2) {
+		printf("안녕히계세요.\n");
+		printf("문을 닫는다.\n");
+		return 0;
 	}
 	
-EXIT:
-	printf("%d\n", num1);
+	if (age < 30) {
+		printf("안녕히계세요.\n");
+		printf("문을 닫는다.\n");
+		return 0;		
+	}
+		
+	if (isOwner < 0) {
+		printf("안녕히계세요.\n");
+		printf("문을 닫는다.\n");	
+		return 0;	
+	}
 	
 	return 0;
 }
