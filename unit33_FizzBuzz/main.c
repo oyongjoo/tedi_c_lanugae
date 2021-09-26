eacd3750a7be656055c5fea17376bab0dadbbe05
Yongjoo Lee <oyongjoo@gmail.com>
2021-09-26 10:57:52 +0900

    fizzbuzz without logical operator

--- unit33_FizzBuzz/main.c
+++ unit33_FizzBuzz/main.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int main()
+{
+    for (int i = 1; i <= 100; i++)   // 1부터 100까지 100번 반복
+    {
+        if (i % 15 == 0)             // 15의 배수(3과 5의 공배수)일 때
+            printf("FizzBuzz\n");    // FizzBuzz 출력
+        else if (i % 3 == 0)         // 3의 배수일 때
+            printf("Fizz\n");        // Fizz 출력
+        else if (i % 5 == 0)         // 5의 배수일 때
+            printf("Buzz\n");        // Buzz 출력
+        else
+            printf("%d\n", i);       // 아무것도 해당되지 않을 때 숫자 출력
+    }
 
-int main() {
-	for (int i = 1; i <= 100; i++) {
-		if (i % 3 == 0 && i % 5 ==0) 
-			printf("FizzBuzz\n");
-		else if (i % 3 == 0)
-			printf("Fizz\n");
-		else if (i % 5 == 0)
-			printf("Buzz\n");
-		else
-			printf("%d\n", i);
-	}
-	return 0;
+    return 0;
 }
