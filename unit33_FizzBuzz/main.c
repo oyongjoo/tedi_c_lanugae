7c200e5c512b48f204ee8d4e3da6e926ff499fe3
Yongjoo Lee <oyongjoo@gmail.com>
2021-09-26 11:18:25 +0900

    fizzbuzz code with terary operator

--- unit33_FizzBuzz/main.c
+++ unit33_FizzBuzz/main.c
@@ -2,17 +2,9 @@
 
 int main()
 {
-    for (int i = 1; i <= 100; i++)   // 1부터 100까지 100번 반복
-    {
-        if (i % 15 == 0)             // 15의 배수(3과 5의 공배수)일 때
-            printf("FizzBuzz\n");    // FizzBuzz 출력
-        else if (i % 3 == 0)         // 3의 배수일 때
-            printf("Fizz\n");        // Fizz 출력
-        else if (i % 5 == 0)         // 5의 배수일 때
-            printf("Buzz\n");        // Buzz 출력
-        else
-            printf("%d\n", i);       // 아무것도 해당되지 않을 때 숫자 출력
-    }
-
+	for (int i = 0; ++i <= 100;)	// 조건식 안에서 변화식을 함께 작성
+		printf(i % 3 ? i % 5 ? "%d\n" : "Buzz\n" : i % 5 ? "Fizz\n" : "BuzzFizz\n", i);
+		// 삼항 연산자를 세 번 사용하여 printf 안에서 처리
+	
     return 0;
 }
