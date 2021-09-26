be7ace2cdab8ff7d9836bd3a544d4af322061e70
Yongjoo Lee <oyongjoo@gmail.com>
2021-09-26 20:10:22 +0900

    unit34 pointer / dereference_assign.c

--- unit34_Pointer/main.c
+++ unit34_Pointer/main.c
@@ -3,14 +3,14 @@
 int main()
 {
     int *numPtr;      // 포인터 변수 선언
-    int num1 = 10;    // int형 변수를 선언하고 10 저장
+    int num1 = 10;    // 정수형 변수를 선언하고 10 저장
 
     numPtr = &num1;   // num1의 메모리 주소를 포인터 변수에 저장
 
-    printf("%p\n", numPtr);    // 0055FC24: 포인터 변수 numPtr의 값 출력
-                               // 컴퓨터마다, 실행할 때마다 달라짐
-    printf("%p\n", &num1);     // 0055FC24: 변수 num1의 메모리 주소 출력
-                               // 컴퓨터마다, 실행할 때마다 달라짐
+    *numPtr = 20;     // 역참조 연산자로 메모리 주소에 접근하여 20을 저장
+
+    printf("%d\n", *numPtr);    // 20: 역참조 연산자로 메모리 주소에 접근하여 값을 가져옴
+    printf("%d\n", num1);       // 20: 실제 num1의 값도 바뀜
 
     return 0;
 }
