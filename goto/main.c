8fe9b6c561536140df3b2061699fb1a49e93a306
Yongjoo Lee <oyongjoo@gmail.com>
2021-09-26 10:41:13 +0900

    goto error pattern

--- goto/main.c
+++ goto/main.c
@@ -14,23 +14,13 @@ int main() {
 	printf("안녕하세요.\n");
 	printf("문을 연다\n");
 	
-	if (gender == 2) {
-		printf("안녕히계세요.\n");
-		printf("문을 닫는다.\n");
-		return 0;
-	}
-	
-	if (age < 30) {
-		printf("안녕히계세요.\n");
-		printf("문을 닫는다.\n");
-		return 0;		
+	if (gender == 2 || age < 30 || isOwner < 0) {
+		goto EXIT:		// 에러가 발생했으므로 EXIT로 이동
 	}
 		
-	if (isOwner < 0) {
-		printf("안녕히계세요.\n");
-		printf("문을 닫는다.\n");	
-		return 0;	
-	}
+EXIT:
+	printf("안녕히계세요.\n");	// 에러 처리 코드를 한 번만 사용함. 
+	printf("문을 닫는다.\n");	
 	
 	return 0;
 }
