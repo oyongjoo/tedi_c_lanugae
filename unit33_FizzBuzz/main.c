9af3d0cd51fbf7d7ad6a6d53b66c53b1de218ee2
Yongjoo Lee <oyongjoo@gmail.com>
2021-09-26 10:57:38 +0900

    delete unnecessary code

--- unit33_FizzBuzz/main.c
+++ unit33_FizzBuzz/main.c
@@ -4,17 +4,14 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main() {
-	for (int i = 1; i <= 20; i++) {
-		if (i % 3 == 0 && i % 5 ==0) {
+	for (int i = 1; i <= 100; i++) {
+		if (i % 3 == 0 && i % 5 ==0) 
 			printf("FizzBuzz\n");
-			continue;
-		} else if (i % 3 == 0) {
+		else if (i % 3 == 0)
 			printf("Fizz\n");
-			continue;
-		} else if (i % 5 == 0) {
+		else if (i % 5 == 0)
 			printf("Buzz\n");
-			continue;
-		} else
+		else
 			printf("%d\n", i);
 	}
 	return 0;
