0981722c6cc4ddbbb6819607028e587abb51ff41
Yongjoo Lee <oyongjoo@gmail.com>
2021-09-26 22:04:06 +0900

    unit34_pointer / const int pointer

--- unit34_Pointer/main.c
+++ unit34_Pointer/main.c
@@ -2,22 +2,14 @@
 
 int main()
 {
-    long long *numPtr1;    // long long형 포인터 선언
-    float *numPtr2;        // float형 포인터 선언
-    char *cPtr1;           // char형 포인터 선언
-
-    long long num1 = 10;
-    float num2 = 3.5f;
-    char c1 = 'a';
-
-    numPtr1 = &num1;    // num1의 메모리 주소 저장
-    numPtr2 = &num2;    // num2의 메모리 주소 저장
-    cPtr1 = &c1;        // c1의 메모리 주소 저장
-
-	printf("%d, %d\n", sizeof(cPtr1), sizeof(*cPtr1));
-    printf("%lld\n", *numPtr1);    // 10
-    printf("%f\n", *numPtr2);      // 3.500000
-    printf("%c\n", *cPtr1);        // a
+	const int num1 = 10;
+	const int num2 = 30;
+	const int *snumPtr = &num1;
+	
+	numPtr = &num2;
+	*numPtr = 20;   
+	
+	printf("%#x, %d\n", numPtr, *numPtr);
 
     return 0;
 }
