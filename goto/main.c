b6d67d3b8c084de8cb075f95154de6ab096c671d
Yongjoo Lee <oyongjoo@gmail.com>
2021-09-26 10:12:17 +0900

    problem: if multiple loop.

--- goto/main.c
+++ goto/main.c
@@ -1,16 +1,25 @@
-#define _CRT_SECURE_MO_WARNINGS
 #include <stdio.h>
+#include <stdbool.h>
 
-/* 아래 코드를 스파게티 코드라고 한다. 불필요한 goto는 자제하자.*/
 int main() {
-	int num1; 
+	int num1 = 0;
 	
-	scanf("%d", &num1);
-	
-    if (num ==1 ) {
-		printf("1입니다.\n");
-	} else if (num == 2) {
-		printf("2입니다.\n");
+	bool exitOuterLoop = false;
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			if (num1 == 20) {
+				exitOuterLoop = true;
+				break;
+			}
+			
+			num1++;
+		}
+		
+		if (exitOuterLoop == true)
+			break;
 	}
+	
+	printf("%d\n", num1);
+	
 	return 0;
 }
