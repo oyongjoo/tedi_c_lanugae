a60a22fb6a26dbcd5a6b46f66d58b02c3535f8b6
Yongjoo Lee <oyongjoo@gmail.com>
2021-09-26 23:39:10 +0900

    unit35_malloc: memory set

--- malloc/main.c
+++ malloc/main.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>    // malloc, free 함수가 선언된 헤더 파일
+#include <string.h>
 
 int main()
 {
-    int *numPtr;
-    
-    numPtr = malloc(sizeof(int));
-    
-    *numPtr = 10;
-    
-    printf("%d\n", *numPtr);
-    
-    free(numPtr);
+	long long *numPtr = malloc(sizeof(long long));
+	
+	memset(numPtr, 0x27, 8);
+	
+	printf("%#llx\n", *numPtr);
+	
+	free(numPtr);
 
     return 0;
 }
