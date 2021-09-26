f69a92f2ae7457001671f3f8a1e92a01b68f7bbb
Yongjoo Lee <oyongjoo@gmail.com>
2021-09-26 21:32:53 +0900

    unit34_pointer / pointer_type.c

--- unit34_Pointer/main.c
+++ unit34_Pointer/main.c
@@ -2,15 +2,21 @@
 
 int main()
 {
-    int *numPtr;      // 포인터 변수 선언
-    int num1 = 10;    // 정수형 변수를 선언하고 10 저장
+    long long *numPtr1;    // long long형 포인터 선언
+    float *numPtr2;        // float형 포인터 선언
+    char *cPtr1;           // char형 포인터 선언
 
-    numPtr = &num1;   // num1의 메모리 주소를 포인터 변수에 저장
+    long long num1 = 10;
+    float num2 = 3.5f;
+    char c1 = 'a';
 
-    *numPtr = 20;     // 역참조 연산자로 메모리 주소에 접근하여 20을 저장
+    numPtr1 = &num1;    // num1의 메모리 주소 저장
+    numPtr2 = &num2;    // num2의 메모리 주소 저장
+    cPtr1 = &c1;        // c1의 메모리 주소 저장
 
-    printf("%d\n", *numPtr);    // 20: 역참조 연산자로 메모리 주소에 접근하여 값을 가져옴
-    printf("%d\n", num1);       // 20: 실제 num1의 값도 바뀜
+    printf("%lld\n", *numPtr1);    // 10
+    printf("%f\n", *numPtr2);      // 3.500000
+    printf("%c\n", *cPtr1);        // a
 
     return 0;
 }
