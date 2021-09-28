1f742eeb716a8e27fc2b53b38ad12fc9bb9ae66c
Yongjoo Lee <oyongjoo@gmail.com>
2021-09-28 20:05:12 +0900

    unit36_array EXERCISE convert binary to decimal

--- array/main.c
+++ array/main.c
@@ -2,28 +2,18 @@
 
 int main()
 {
-    int decimal = 13;
-    int binary[20] = { 0, };
-
-    int position = 0;
-    while (1)
-    {
-        binary[position] = decimal % 2;    // 2로 나누었을 때 나머지를 배열에 저장
-        decimal = decimal / 2;             // 2로 나눈 몫을 저장
-
-        position++;    // 자릿수 변경
-
-        if (decimal == 0)    // 몫이 0이 되면 반복을 끝냄
-            break;
-    }
-
-    // 배열의 요소를 역순으로 출력
-    for (int i = position - 1; i >= 0; i--)
-    {
-        printf("%d", binary[i]);
-    }
-
-    printf("\n");
-
+	int decimal = 0;
+	int binary[4] = {1,1,0,1}; // 1101 순서대로 저장됨
+	
+	int position = 0;
+	for (int i = sizeof(binary) / sizeof(int) - 1; i >= 0; i--) {
+		if (binary[i] == 1)
+			decimal += 1 << position;
+			
+		position++;
+	}
+	
+	printf("%d\n", decimal);
+	
     return 0;
 }
